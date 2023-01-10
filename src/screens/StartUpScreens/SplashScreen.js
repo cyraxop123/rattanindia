b377d677860dfca55193b520b1a1c76a63648805
Cyrax <lyfissed@gmail.com>
2023-01-10 22:25:40 +0530

    test

--- src/screens/StartUpScreens/SplashScreen.js
+++ src/screens/StartUpScreens/SplashScreen.js
@@ -1,28 +1,49 @@
-import { StyleSheet, Text, View, SafeAreaView, Image, StatusBar, Dimensions} from 'react-native'
-import React, { useEffect } from 'react'
+import { StyleSheet, Text, View, SafeAreaView, Image, StatusBar, Dimensions, Alert } from 'react-native'
+import React, { useEffect, useCallback } from 'react'
 import { useNavigation, StackActions } from '@react-navigation/native'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
-const {width} = Dimensions.get("window")
+const { width } = Dimensions.get("window")
 
 const SplashScreen = () => {
 
   const navigation = useNavigation();
 
+  const checkStatus = useCallback(
+    async () => {
+      setTimeout(async() => {
+        try {
+          const value = await AsyncStorage.getItem('token')
+          if (value !== null) {
+            Alert("ok")
+            navigation.dispatch(
+              StackActions.replace("Bottom")
+            )
+          }else{
+            navigation.dispatch(
+              StackActions.replace("OnBoarding")
+            )
+          }
+        } catch (e) {
+          console.log("ok")
+        }
+      }, 3000);
+    },
+    [],
+  )
+
+
   useEffect(() => {
-    setTimeout(() => {
-      navigation.dispatch(
-        StackActions.replace("OnBoarding")
-      )
-    }, 3000);
+    checkStatus()
   }, [])
-  
+
 
   return (
     <SafeAreaView style={styles.container}>
-        <StatusBar backgroundColor={"#002f09"}/>
+      <StatusBar backgroundColor={"#002f09"} />
       <View style={styles.logoView}>
-        <Image style={styles.logo} source={require('../../../assets/images/app-logo.png')}/>
+        <Image style={styles.logo} source={require('../../../assets/images/app-logo.png')} />
       </View>
     </SafeAreaView>
   )
@@ -31,22 +52,22 @@ const SplashScreen = () => {
 export default SplashScreen
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1, 
-        backgroundColor: "#002f09"
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#002f09"
+  },
 
-    logoView: {
-        flex: 1.2,
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 30,
-    },
+  logoView: {
+    flex: 1.2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 30,
+  },
 
-    logo: {
-        resizeMode: 'contain',
-        width: width * 0.58
-    },
+  logo: {
+    resizeMode: 'contain',
+    width: width * 0.58
+  },
 
 
 })
